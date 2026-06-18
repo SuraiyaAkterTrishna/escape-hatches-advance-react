e912e6294d43bb29fd32926e761c17ac5d7289d9
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2026-06-18 20:14:35 +0600

    useRef

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Counter from "./Counter"
+
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello React...</h1>
+      <Counter />
+    </>
+  )
+}
+
+export default App
---
+++ src/Counter.jsx
@@ -0,0 +1,13 @@
+import { useRef } from "react";
+
+export default function Counter(){
+    let ref = useRef(0);
+
+    function handleClick(){
+        ref.current = ref.current + 1;
+        console.log(ref.current)
+    }
+    return (
+        <button onClick={handleClick}>Click Me!!!</button>
+    );
+}
