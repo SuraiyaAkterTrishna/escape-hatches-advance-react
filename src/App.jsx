6bb477314cb390eabb47f587e1ccfefaf681066b
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2026-07-01 11:58:58 +0600

    avoid effect and use a key to resetting state on prop change

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 /* eslint-disable no-unused-vars */
-import Comments from "../components/Comments"
-import Example1 from "../components/Example1"
-
+import { useState } from "react";
+import Comments from "../components/Comments";
+import Example1 from "../components/Example1";
+import Example2 from "../components/Example2";
 
 function App() {
+  const [userId, setUserId] = useState(1);
 
   return (
     <>
-     <Example1 />
+      {/* <Example1 /> */}
+
+      <div>
+        <button onClick={() => setUserId(userId === 1 ? 2 : 1)}>
+          Switch to Profile No. {userId === 1 ? 2 : 1}
+        </button>
+      </div>
+      <Example2 key={userId} userId={userId} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
