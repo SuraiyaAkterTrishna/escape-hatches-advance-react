50fdbc77546178c55e7badda15c7f6b21903f41d
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2026-07-01 12:03:25 +0600

    Avoid: Adjusting state on prop change in an Effect  Best: Calculate everything during rendering

---
+++ components/Example3.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+export default function Example3({ items }) {
+    const [selectedId, setSelectedId] = useState(null);
+
+    // 🔴 Avoid: Adjusting state on prop change in an Effect
+    // ✅ Best: Calculate everything during rendering
+
+    const selection = items.find((item) => item.id === selectedId) ?? null;
+
+    return (
+        <>
+            <ul>
+                {items.map((item) => (
+                    <li key={item.id}>
+                        <input
+                            type="checkbox"
+                            checked={item.id === selection?.id}
+                            onChange={() => setSelectedId(item.id)}
+                        />{" "}
+                        {item.title}
+                    </li>
+                ))}
+            </ul>
+            {selection?.id && (
+                <div>
+                    <h2>Selected Item</h2>
+                    <div
+                        style={{
+                            padding: "10px",
+                            backgroundColor: "lightcyan",
+                        }}
+                    >
+                        {selection?.title}
+                    </div>
+                </div>
+            )}
+        </>
+    );
+}
--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,27 @@ import { useState } from "react";
 import Comments from "../components/Comments";
 import Example1 from "../components/Example1";
 import Example2 from "../components/Example2";
+import { items1, items2 } from "./data/itemsData";
+import Example3 from "../components/Example3";
 
 function App() {
-  const [userId, setUserId] = useState(1);
+  const [items, setItems] = useState(items1);
+  // const [userId, setUserId] = useState(1);
 
   return (
     <>
       {/* <Example1 /> */}
 
-      <div>
+      {/* <div>
         <button onClick={() => setUserId(userId === 1 ? 2 : 1)}>
           Switch to Profile No. {userId === 1 ? 2 : 1}
         </button>
       </div>
-      <Example2 key={userId} userId={userId} />
+      <Example2 key={userId} userId={userId} /> */}
+
+      <button onClick={() => setItems(items2)}>Switch to Items2</button>
+      <button onClick={() => setItems(items1)}>Switch to Items1</button>
+      <Example3 items={items} />
     </>
   );
 }
